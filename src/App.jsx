e9bc56389257f12f9e5845e7af76a2d3ec26e0cd
Nwafor Emmanuel <emmanuelnwaforce@gmail.com>
2023-08-08 13:22:01 +0100

    template

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div>
+      <h1>nav</h1>
+      <h1>HeroSection</h1>
+      <h1>About</h1>
+      <h1>Projects</h1>
+      <h1>Skills</h1>
+      <h1>Contacts</h1>
+     </div>
+    </div>
+  );
+}
+
+export default App;
